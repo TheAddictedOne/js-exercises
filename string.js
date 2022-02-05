f75f5a6e6c9951479488b6859aa69a5786a847d4
Marie Sommier <marie.a.s45@gmail.com>
2022-02-05 21:34:31 +0100

    exo1 : done

--- string.js
+++ string.js
@@ -1,9 +1,22 @@
 import ERROR from "./errors.js";
 
-export default { charAt };
 
 /**
  * @param {string} str
  * @param {number} index
  */
-function charAt(str, index) {}
+const charAt = (str,index) => {
+    //quand l'index est supérieur à la taille de str return error
+    // quand l'index inférieur à 0, error 
+    // sinon, renvoie str[index]
+    if (index >= str.length ) {
+        return ERROR.OUT_OF_BOUND
+    }
+    if (index < 0) {
+        return ERROR.OUT_OF_BOUND
+    }
+    return str[index]
+
+}
+
+export default { charAt };
